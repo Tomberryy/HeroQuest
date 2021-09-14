a77088aac98dd4058993526fb99003d7bcfc6454
Tomberryy <antoine.monnin@gmail.com>
2021-09-14 13:22:46 +0200

    True game object
    Not more global variables
    Game is a constant
    Every variable is local to game object

--- Classes/game.js
+++ Classes/game.js
@@ -1,15 +1,24 @@
 class Game {
 
+    heros = null;
+    weapons = [];
+    armors = [];
+    npcs = null;
+    monsters = [];
+    currentMonster = null;
+    eventCount = 0;
+
+
     init() {
-        monsters[0] = new Monster("Goblin", 10, 10, 10);
-        monsters[1] = new Monster("Orc", 20, 12, 12);
-        monsters[2] = new Monster("Undead", 15, 2, 15);
-        weapons[0] = new Weapon("Club", 3, 5);
-        weapons[1] = new Weapon("Sword", 5, 10);
-        weapons[2] = new Weapon("Axe", 8, 15);
-        armors[0] = new Armor("Leather armor", 2, 5);
-        armors[1] = new Armor("Chainmail", 5, 10);
-        armors[2] = new Armor("Plate armor", 10, 25);
+        this.monsters[0] = new Monster("Goblin", 10, 10, 10);
+        this.monsters[1] = new Monster("Orc", 20, 12, 12);
+        this.monsters[2] = new Monster("Undead", 15, 2, 15);
+        this.weapons[0] = new Weapon("Club", 3, 5);
+        this.weapons[1] = new Weapon("Sword", 5, 10);
+        this.weapons[2] = new Weapon("Axe", 8, 15);
+        this.armors[0] = new Armor("Leather armor", 2, 5);
+        this.armors[1] = new Armor("Chainmail", 5, 10);
+        this.armors[2] = new Armor("Plate armor", 10, 25);
     }
 
 
@@ -22,19 +31,19 @@ class Game {
     newGame() {
 
         //Get value from hero name textbox
-        var heroName = document.getElementById('heroName').value;
+        let heroName = document.getElementById('heroName').value;
 
         //If textbox isn't empty
         if (heroName !== "") {
 
             //Create new hero in global variable "heros"
-            heros = new Hero(heroName, 100, 0, 10, 5, 5, 1, 1, 0, 0);
-
+            this.heros = new Hero(heroName, 100, 0, 10, 5, 5, 1, 1, 0, 0);
+            console.log(this.heros);
             //Change game state to two (2 = combat or shop)
-            this.changeState(2, "fight");
+            this.changeState(2);
 
             //First event
-            eventCount++;
+            this.eventCount++;
 
         } else { //Else nope
             alert("Nan mais allo!");
@@ -42,7 +51,7 @@ class Game {
         }
     }
 
-    changeState(state, event) {
+    changeState(state) {
 
         //get all scenes elements
         let menuScene = document.getElementById('menuScene');
@@ -57,16 +66,16 @@ class Game {
                 menuScene.style.display = "block";
                 break;
 
-            //Case 2 is combat / shop state
+            //Case 2 is combat
             case 2:
                 menuScene.style.display = "none";
                 scoreScene.style.display = "none";
                 playScene.style.display = "block";
                 document.getElementById("shopButtons").style.display = "none";
-                currentMonster = monsters[this.rand(2)]
-                this.refreshFight(heros, currentMonster);
+                //Create new monster
+                this.currentMonster = {...this.monsters[this.rand(2)]};
+                this.refreshFight(1);
                 break;
-
             //Case 3 is score screen
             case 3:
                 playScene.style.display = "none";
@@ -76,54 +85,73 @@ class Game {
         }
     }
 
-    //Refreshes all the informations before and during fight
-    refreshFight(hero, monster) {
-        //Get both stats divs
-        let heroStats = document.getElementById('charStats');
-        let monsterStats = document.getElementById('monsterStats');
+    //Refreshes all the informations before and during fight / shop
+    //event 1 = fight, event 2 = shop
+    refreshFight(event) {
+        switch (event) {
+            case 1:
+                //Get both stats divs
+                let heroStats = document.getElementById('charStats');
+                let monsterStats = document.getElementById('monsterStats');
 
-        currentMonster = monster;
-        heros = hero;
+                //Refresh hero stats
+                heroStats.innerHTML = "<p>" + this.heros.name +
+                    "<br/>HP : " + this.heros.hp +
+                    "<br/>Gold : " + this.heros.gold +
+                    "<br/>xp : " + this.heros.xp +
+                    "<br/>ATK : " + this.heros.atk +
+                    "<br/>HP : " + this.heros.def +
+                    "<br/>str : " + this.heros.str +
+                    "<br/>end : " + this.heros.end + "</p>";
 
-        //Refresh hero stats
-        heroStats.innerHTML = "<p>" + hero.name +
-            "<br/>HP : " + hero.hp +
-            "<br/>Gold : " + hero.gold +
-            "<br/>xp : " + hero.xp +
-            "<br/>ATK : " + hero.atk +
-            "<br/>HP : " + hero.def +
-            "<br/>str : " + hero.str +
-            "<br/>end : " + hero.end + "</p>";
+                //Refresh monster stats
+                monsterStats.innerHTML = "<p>" + this.currentMonster.name +
+                    "<br/>HP : " + this.currentMonster.hp +
+                    "<br/>ATK : " + this.currentMonster.atk + "</p>";
+                break;
+            case 2:
+                //Get shop window
+                let shopInv = document.getElementById('monsterStats');
 
-        //Refresh monster stats
-        monsterStats.innerHTML = "<p>" + monster.name +
-            "<br/>HP : " + monster.hp +
-            "<br/>ATK : " + monster.atk + "</p>";
+                //Display shop buttons, hide fight buttons
+                document.getElementById("shopButtons").style.display = "block";
+                document.getElementById("fightButtons").style.display = "none";
+
+                //Display shop content
+                shopInv.innerHTML = this.npcs.name + "<br/><br/>" +
+                    this.weapons[this.npcs.weapon].name + "<br/>+" +
+                    this.weapons[this.npcs.weapon].bonus + "atk<br/>Cost : " +
+                    this.weapons[this.npcs.weapon].prix + " gold <br/><br/>" +
+                    this.armors[this.npcs.armor].name + "<br/>+" +
+                    this.armors[this.npcs.armor].bonus + "def<br/>Cost : " +
+                    this.armors[this.npcs.armor].prix + " gold";
+                break;
+        }
     }
 
-    heroAction(action, hero, monster) {
+    heroAction(action) {
         let damageH = null;
         let damageM = null;
         switch (action) {
             case 1: //Hero and monster attack
 
                 //Calculate hero and monster attack
-                damageH = hero.atk + hero.str + weapons[hero.weapon].bonus;
-                damageM = monster.atk - hero.def - hero.end - armors[hero.armor].bonus;
+                damageH = this.heros.atk + this.heros.str + this.weapons[this.heros.weapon].bonus;
+                damageM = this.currentMonster.atk - this.heros.def - this.heros.end - this.armors[this.heros.armor].bonus;
 
                 //If the hero would not kill the monster next swing
-                if (this.isDead(monster, damageH)) {
+                if (this.isDead(this.currentMonster, damageH)) {
 
                     //Lower monster HP
-                    monster.hp = monster.hp - damageH;
+                    this.currentMonster.hp = this.currentMonster.hp - damageH;
 
                     //If the hero doesn't get killed by next swing
-                    if (this.isDead(hero, damageM)) {
+                    if (this.isDead(this.heros, damageM)) {
 
                         //Lower hero HP
-                        hero.hp = hero.hp - damageM;
+                        this.heros.hp = this.heros.hp - damageM;
                         //Refresh fight values
-                        this.refreshFight(hero, monster);
+                        this.refreshFight(1);
 
                     } else { //Hero is dead
 
@@ -131,42 +159,42 @@ class Game {
                 } else { //Monster is dead
 
                     //1 out of 3 events is a shop
-                    if (eventCount % 3 !== 0) {
+                    if (this.eventCount % 3 !== 0) {
 
                         //New event
-                        eventCount++;
+                        this.eventCount++;
                         //Gold won from fight
-                        hero.gold = hero.gold + monster.gold;
+                        this.heros.gold = this.heros.gold + this.currentMonster.gold;
                         //Refresh monsters values
-                        this.init();
                         //New monster
-                        monster = monsters[this.rand(2)];
+                        this.currentMonster = {...this.monsters[this.rand(2)]};
                         //New fight
-                        this.refreshFight(hero, monster);
+                        this.refreshFight(1);
 
                     } else { //Is a shop
-                        this.refreshFight(hero, monster);
-                        npcs = new Npc("Joe", this.rand(2), this.rand(2));
-                        this.showShop(npcs);
+                        this.refreshFight(1);
+                        this.eventCount = 0;
+                        this.npcs = new Npc("Joe", this.rand(2), this.rand(2));
+                        this.refreshFight(2);
                     }
                 }
                 break;
 
             case 2: //Hero defends, monster attacks
-                
+
                 //Maths for monster damage
-                damageM = monster.atk - hero.def - hero.end - armors[hero.armor].bonus;
+                damageM = this.currentMonster.atk - this.heros.def - this.heros.end - this.armors[this.heros.armor].bonus;
 
                 //The hero defends
                 damageM = floor(damageM / 2);
 
                 //If the hero doesn't get killed by next swing
-                if (this.isDead(hero, damageM)) {
+                if (this.isDead(this.heros, damageM)) {
 
                     //Lower hero HP
-                    hero.hp = hero.hp - damageM;
+                    this.heros.hp = heros.hp - damageM;
                     //Refresh fight values
-                    this.refreshFight(hero, monster);
+                    this.refreshFight(1);
                 } else {
 
                 }
@@ -176,6 +204,8 @@ class Game {
         }
     }
 
+    //function returns false if the person would die from swing
+    //Else true
     isDead(person, dmg) {
         if (person.hp - dmg < 0) {
             return false;
@@ -184,25 +214,26 @@ class Game {
         }
     }
 
-    showShop(npc) {
-        let shopInv = document.getElementById('monsterStats');
-        document.getElementById("shopButtons").style.display = "block";
-        document.getElementById("fightButtons").style.display = "none";
-        shopInv.innerHTML = npcs.name + "<br/><br/>" +
-                            weapons[npc.weapon].name + "<br/>+" +
-                            weapons[npc.weapon].bonus + "atk<br/>Cost : " +
-                            weapons[npc.weapon].prix + " gold <br/><br/>" +
-                            armors[npc.armor].name + "<br/>+" +
-                            armors[npc.armor].bonus + "def<br/>Cost : " +
-                            armors[npc.armor].prix + " gold";
-    }
+    buyWeapon(npc) {
+        if (this.heros.gold > this.armors[this.npcs.armor].prix) {
+            //Removegold
+            this.heros.gold = this.heros.gold - this.armors[this.npcs.armor].prix;
+            //Hero gets weapon
+            this.heros.weapon = this.npcs.weapon;
+            console.log("arme");
+            //Disable weapon button
+            document.getElementById('buyWeapon').display = "none";
+        } else {
 
-    buyWeapon() {
-
+        }
     }
 
     buyArmor() {
-
+        //Hero gets armor
+        this.heros.armor = this.npcs.weapon;
+        console.log("armor");
+        //Disable armor button
+        document.getElementById('buyArmor').display = "none";
     }
 
     leaveShop() {
--- Classes/init.js
+++ Classes/init.js
@@ -1,8 +1,2 @@
-var heros = null;
-var weapons = [];
-var armors = [];
-var npcs = [];
-var monsters = [];
-var currentMonster = null;
-var eventCount = 0;
-var game = new Game();
+const game = new Game();
+game.init();
